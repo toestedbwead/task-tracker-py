0144c422e59b72f72219f03900f28145c59f2435
toestedbwead <leielcano@gmail.com>
2025-11-13 22:10:09 +0800

    in the middle of doing the delete command. add command is working well.

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Task Tracker CLI using Python
 
 
+[text](https://roadmap.sh/projects/task-tracker)
--- task_cli.py
+++ task_cli.py
@@ -1,4 +1,100 @@
-import sys # contains all command-line arguments
+import sys
+import json
+import os
+from datetime import datetime
+
+TASKS_FILE = 'tasks-list.json'
+
+# load tasks from JSON file
+def load_tasks():
+    if not os.path.exists(TASKS_FILE):
+        return []
+    try:
+        with open(TASKS_FILE, 'r') as file:
+            return json.load(file)
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON file. Starting with an empty tasks.")
+        return []
+    
+# save tasks from JSON file
+
+def save_tasks(tasks):
+    try:
+        with open(TASKS_FILE, 'w') as file:
+            json.dump(tasks, file, indent=4)
+    except Exception as e:
+        print(f"Error saving tasks: {e}")
 
 
+# main function to handle cli commands
+def main():
+    print("CLI Task Tracker")
+    print("Command List: add, update, delete")
+    if len(sys.argv) < 2:
+        print("Usage: python task_cli.py <command> [args]")
+        return
 
+    command = sys.argv[1]
+    tasks = load_tasks()
+
+    if command == 'add':
+        if len(sys.argv) < 3:
+            print("Error: Please provide a task description.")
+            return
+        description = sys.argv[2]
+
+        # put a try block here where the user inputs the wrong command
+
+        # generate unique id
+        if tasks:
+            highest_id = 0
+            for task in tasks:
+                if task['id'] > highest_id:
+                    highest_id = task['id']
+            new_id = highest_id + 1
+        else:
+            new_id = 1
+
+
+        # create tasks
+        now = datetime.now().isoformat()
+        new_task = {
+        'id': new_id,
+        'description': description,
+        'status': 'todo',
+        'createdAt': now,
+        'updatedAt': now
+        }
+
+        tasks.append(new_task)
+        print(f"Task added succesfully (ID: {new_id})")
+
+        save_tasks(tasks)
+    
+    elif command == 'delete':
+        if len(sys.argv) < 3:
+            print("Error: Please provide a task id that you want to delete.")
+            return
+        delete_task_id = int(sys.argv[2]) + 1
+
+
+        found_index = -1
+        for index, task in enumerate(tasks):
+            if task['id'] == delete_task_id:
+                found_index = index
+                break
+
+        if found_index == - 1:
+            print(f"Error: Task with ID {delete_task_id} not found.")
+            return 
+        
+
+        deleted_task = tasks.pop(found_index)
+        print(f"Task deleted succesfully (ID: {deleted_task['id']} )")
+
+        save_tasks(tasks)
+        
+
+
+if __name__ == "__main__":
+    main() 
